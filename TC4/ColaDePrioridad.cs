e2f934d274e36ae9a25478fa131090cff17a5d6b
valarcon29011995 <92767134+valarcon29011995@users.noreply.github.com>
2021-11-29 08:23:32 -0500

    Add files via upload

---
+++ TC4/ColaDePrioridad.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TC4
+{
+    internal class ColaDePrioridad
+    {
+        public void RecorrerCola(Nodo header)
+        {
+            Nodo temp = new Nodo();
+            while (!isEmpty(header))
+            {
+                Console.WriteLine("ID: " + header.dato.id + " , " + header.dato.nombre + " " +
+                    header.dato.apellido + " , Prioridad: " + header.dato.prioridad);
+                temp = header;
+                header = header.next;
+            }
+            Console.WriteLine();
+        }
+
+        public Nodo NuevoNodo(Paciente dato, int prioridad)
+        {
+            Nodo temp = new Nodo();
+            temp.dato = dato;
+            temp.dato.prioridad = prioridad;
+            temp.next = null;
+            return temp;
+        }
+
+        public Nodo push(Nodo header, Paciente dato, int prioridad)
+        {
+            Nodo puntero = header;
+            Nodo temp = NuevoNodo(dato, prioridad);
+            if (header.dato.prioridad > prioridad)
+            {
+                temp.next = header;
+                header = temp;
+            }
+            else
+            {
+                while (puntero.next != null && puntero.next.dato.prioridad < prioridad)
+                {
+                    puntero = puntero.next;
+                }
+                temp.next = puntero.next;
+                puntero.next = temp;
+            }
+            return header;
+        }
+
+        public Nodo pop(Nodo header)
+        {
+            Nodo temp = header;
+            (header) = (header).next;
+            return header;
+        }
+
+        public bool isEmpty(Nodo header)
+        {
+            if (header == null) return true;
+            return false;
+        }
+    }
+}
---
+++ TC4/Nodo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TC4
+{
+    internal class Nodo
+    {
+        public Paciente dato;
+        public Nodo next;
+    }
+}
